a6a86a78533c0e6b6dbdfc89d1475bfe8ae007a3
arthas_sjtu <arthas_sjtu@163.com>
2017-10-31 22:10:27 +0800

    将time wheel初步整合进入jgroups框架

--- src/main/java/state/Follower.java
+++ src/main/java/state/Follower.java
@@ -1,17 +1,37 @@
 package state;
 
+import Utils.Timeout;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.View;
 import worker.MainWorker;
 
+import Utils.Timeout;
+import Utils.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by jrj on 17-10-30.
  */
 public class Follower extends State {
+    Timeout timeoutForCandidate;
+    TimerTask timerTask;
     public Follower(){
         curState = FOLLOWER;
-        Message message = new Message(null,jChannel.getAddress(),jChannel.getAddressAsString()+";Join in Group");
+        timerTask = new TimerTask() {
+            public void run(Timeout timeout) throws Exception {
+                Message message = new Message(null,jChannel.getAddress(),"2:"+jChannel.getAddressAsString()+";Becoming:candidate;term:"+ term++);
+                jChannel.send(message);
+                System.out.println("I am candidate");
+                //这里有问题,如果执行到这里外边执行timeoutForCandidate.cancel()就会引发错误
+                timeoutForCandidate = hashedWheelTimer.newTimeout(timerTask,3000, TimeUnit.MILLISECONDS);
+            }
+        };
+    }
+
+    public void joinGroup(){
+        Message message = new Message(null,jChannel.getAddress(),"1:"+jChannel.getAddressAsString()+";Join in Group");
+        timeoutForCandidate = hashedWheelTimer.newTimeout(timerTask,3000, TimeUnit.MILLISECONDS);
         try {
             jChannel.send(message);
         } catch (Exception e) {
@@ -20,7 +40,7 @@ public class Follower extends State {
     }
 
     public void fireWhenViewAccepted(View new_view, MainWorker mainWorker) {
-
+        System.out.println("currentSize " + new_view.size());
     }
 
     public void fireWhenMessageReceived(Message msg, MainWorker mainWorker) {
--- src/main/java/state/State.java
+++ src/main/java/state/State.java
@@ -1,5 +1,6 @@
 package state;
 
+import Utils.HashedWheelTimer;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.View;
@@ -13,12 +14,11 @@ public abstract class State {
     protected static int CANDIDATE = 1;
     protected static int LEADER = 2;
     protected static long term = 0;
-
+    protected static HashedWheelTimer hashedWheelTimer = new HashedWheelTimer();
     protected int curState;
     public static void setjChannel(JChannel jChannel){
         State.jChannel = jChannel;
     }
-
     public abstract void fireWhenViewAccepted(View new_view,MainWorker mainWorker);
     public abstract void fireWhenMessageReceived(Message msg,MainWorker mainWorker);
 }
